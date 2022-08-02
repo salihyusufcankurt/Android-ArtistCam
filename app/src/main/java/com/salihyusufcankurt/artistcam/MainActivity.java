88109d03e5c1337e10524f01490243f31b7cef03
S. Yusuf <salihyusufcankurt@hotmail.com>
2022-08-02 09:47:33 +0300

    initial commit

---
+++ app/src/main/java/com/salihyusufcankurt/artistcam/MainActivity.java
@@ -0,0 +1,179 @@
+package com.salihyusufcankurt.artistcam;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    private PreviewView previewView;
+    private VerticalSeekBar verticalSeekBar;
+    private int imageTransparencyValue;
+    private static final int CAMERA_REQUEST = 100;
+    private ImageView imageView;
+    private Button importButton;
+    int SELECT_PICTURE = 200;
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Hide title bar
+        getSupportActionBar().hide();
+
+        //Transparent bottom bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        //Check the camera permission
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST);
+        }
+
+        //UI declerations
+        previewView = findViewById(R.id.previewView);
+        verticalSeekBar = findViewById(R.id.verticalSeekBar);
+        imageView = findViewById(R.id.imageView);
+        importButton = findViewById(R.id.import_button);
+
+        //Set default value for seekbar
+        verticalSeekBar.setProgress(imageTransparencyValue);
+        verticalSeekBar.setProgress(50);
+        verticalSeekBar.setMax(255);
+
+        verticalSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                imageTransparencyValue = verticalSeekBar.getProgress();
+                imageView.setImageAlpha(imageTransparencyValue);
+                //imageView.setAlpha(imageTransparencyValue);
+                System.out.println(imageTransparencyValue);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                //Seek barı opak hale getir.
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                //Seek barı kısmen saydam hale getir.
+            }
+        });
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
+            }
+        }, ContextCompat.getMainExecutor(this));
+
+        importButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageChooser();
+            }
+        });
+    }
+/**
+    textButton.setOnClickListener {
+        // Respond to button press
+    }
+ oncreate dışında onClick gösterimi
+*/
+    private void bindPreview(ProcessCameraProvider cameraProvider) {
+        Preview preview = new Preview.Builder()
+                .build();
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_REQUEST) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "CameraX permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "CameraX permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
+    // this function is triggered when user
+    // selects the image from the imageChooser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (requestCode == SELECT_PICTURE) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    imageView.setImageURI(selectedImageUri);
+                    this.verticalSeekBar.setProgress(255); // uygulama açıldığında ilk defa image seçildiğinde görsel %100 opak geliyor. Burada verdiğim değer gelmiyor.
+                }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/salihyusufcankurt/artistcam/VerticalSeekBar.kt
@@ -0,0 +1,68 @@
+package com.salihyusufcankurt.artistcam
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.appcompat.widget.AppCompatSeekBar
+import androidx.core.content.ContextCompat
+
+/**
+ * Vertical Progress Bar For Android Apps
+ */
+class VerticalSeekBar : AppCompatSeekBar {
+
+    constructor(context: Context) : super(context) {
+        initView(context)
+    }
+    constructor(
+            context: Context,
+            attrs: AttributeSet?,
+            defStyle: Int) : super(context, attrs, defStyle) {
+        initView(context)
+    }
+    constructor(
+            context: Context,
+            attrs: AttributeSet?) : super(context, attrs) {
+        initView(context)
+    }
+
+    private fun initView(context: Context) {
+                                                                    // Set Custom Progress Bar Drawable
+        progressDrawable = ContextCompat.getDrawable(context, R.drawable.vertical_progress_selector)
+        // Set Custom Thumb
+        thumb = ContextCompat.getDrawable(context, R.drawable.vertical_progress_thumb)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int){
+        super.onSizeChanged(h, w, oldh, oldw)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec)
+        setMeasuredDimension(measuredHeight, measuredWidth)
+    }
+
+    override fun onDraw(c: Canvas) {
+        c.rotate(-90f)
+        c.translate(-height.toFloat(), 0f)
+        super.onDraw(c)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (!isEnabled) {
+            return false
+        }
+        when (event.action) {
+            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE, MotionEvent.ACTION_UP -> {
+                progress = max - (max * event.y / height).toInt()
+                onSizeChanged(width, height, 0, 0)
+            }
+            MotionEvent.ACTION_CANCEL -> {
+            }
+        }
+        return true
+    }
+}
